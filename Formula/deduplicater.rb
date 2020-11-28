5d265577d1085cb5aa6fac5cd74af7f3d010e578
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-28 23:20:06 +0000

    Brew formula update for deduplicater version v0.2

--- Formula/deduplicater.rb
+++ Formula/deduplicater.rb
@@ -2,20 +2,20 @@
 class Deduplicater < Formula
   desc "Find and manage duplicate files."
   homepage "https://github.com/driessamyn/deduplicater"
-  version "0.1.1"
+  version "0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/driessamyn/deduplicater/releases/download/v0.1.1/deduplicater_0.1.1_darwin_amd64.tar.gz"
-    sha256 "c10daabcd208f428be84d70a75b7348f7a94e888ba44283973d9f56ee0b60195"
+    url "https://github.com/driessamyn/deduplicater/releases/download/v0.2/deduplicater_0.2_darwin_amd64.tar.gz"
+    sha256 "24b4eb3cd94d3125ab531f5a5b6b91daf3191d7c17cbad196ab6a6154e6e50d5"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/driessamyn/deduplicater/releases/download/v0.1.1/deduplicater_0.1.1_linux_amd64.tar.gz"
-    sha256 "65bcd4016e6c725f79bf8a70b3d30b6b1b2632b91162918f056bc80e109bc530"
+    url "https://github.com/driessamyn/deduplicater/releases/download/v0.2/deduplicater_0.2_linux_amd64.tar.gz"
+    sha256 "fd66114513841484a5ca5616b2d4c9ec02e2f1380c384e25f4faeb78d523da92"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/driessamyn/deduplicater/releases/download/v0.1.1/deduplicater_0.1.1_linux_arm64.tar.gz"
-    sha256 "0a813133be57e91cbb5c82c3f8508510b378ef05b1fe2816961bb8f6af52578a"
+    url "https://github.com/driessamyn/deduplicater/releases/download/v0.2/deduplicater_0.2_linux_arm64.tar.gz"
+    sha256 "81a2fbb676228b371cdb1347e2e5c61ef8d43a7470102b4cc076dbe7a67c98f0"
   end
 
   def install
